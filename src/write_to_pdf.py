20fa9556b6c5670e2c60e6f75e9bda7de33f55d6
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-03-20 23:27:56 +0900

    month,dayの最初の0を削除

--- src/write_to_pdf.py
+++ src/write_to_pdf.py
@@ -45,9 +45,9 @@ def write_application_pdf(date, horse_name, horse_gender, horse_color, horse_age
     date_y = 397
     # テキストを入力
     texts_with_positions = [
-        (year, (170, date_y)),
-        (month, (235, date_y)),
-        (day, (285, date_y)),
+        (year, (167, date_y)),
+        (str(int(month)), (235, date_y)),
+        (str(int(day)), (285, date_y)),
         (horse_name, (90, 265)),
         (horse_color, (270, 240)),
         (horse_age, (130, 240)),
