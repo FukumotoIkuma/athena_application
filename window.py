0f0403af9ccc44d3752fd83403c430efa6991f8c
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-03-21 14:16:13 +0900

    とりあえず--onefileで起動するようになった

--- window.py
+++ window.py
@@ -2,32 +2,43 @@
 import pypdf
 import reportlab
 import pandas
-# 以下のコードは、Flaskとwebviewを使ってGUIアプリケーションを作成するためのコード
+
+
+from flask import Flask, request, render_template_string
 import os
-import webview
-from flask import Flask, render_template, request
 import shutil
+import webview
+import threading
+
 import main
-import sys
-
-# ROUTE_PATH = sys.path[1] if 2 == len(sys.path) else '.'
-# TEMPLATES_PATH = ROUTE_PATH + '/templates'
-
-# app = Flask(name, template_folder=TEMPLATES_PATH)
-
+from html_template import HTML_TEMPLATE
 
 app = Flask(__name__)
+
+import os
+import sys
+
+if getattr(sys, 'frozen', False):
+    # PyInstaller の場合（--onefile で一時フォルダに展開される）
+    BASE_DIR = sys._MEIPASS
+else:
+    # 通常の Python 実行時
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+print("Application directory:", BASE_DIR)
+
+
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    try:
-        return render_template('index.html')
-    except Exception as e:
-        # ここでエラーが発生した場合、スクリプトを強制的に終了する
-        sys.exit(f"An error occurred while rendering the template: {e}")
+    # dir構造を把握するため一時的にここで表示する
+    # base_dirで何が表示されるか確認する
+    dir = os.listdir(BASE_DIR)
+    return render_template_string(HTML_TEMPLATE, message= f"{dir}")
+   
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
@@ -36,7 +47,7 @@ def upload_file():
         assert file.filename != '', "ファイルが選択されていません"
         assert file.filename.endswith(('.xlsx', '.xlsm')), "ファイル形式が間違っています"
     except AssertionError as e:
-        return render_template('index.html', message=e)
+        return render_template_string(HTML_TEMPLATE, message= f"{e}")
     
     if file:
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
@@ -44,14 +55,22 @@ def upload_file():
         try:
             main.main(file_path)
         except Exception as e:
-            return render_template('index.html', message=f"不明なエラー: {e}")
+            return render_template_string(HTML_TEMPLATE, message= f"不明なエラー: {e}")
 
-        return render_template('index.html', message=f"変換完了. ダウンロードフォルダを確認してください")
+        return render_template_string(HTML_TEMPLATE, message="変換完了. ダウンロードフォルダを確認してください")
+    
+
 if __name__ == '__main__':
-    # Flaskアプリケーションをバックグラウンドで実行
-    from threading import Thread
-    port = 5001
-    thread = Thread(target=lambda: app.run(port=port,debug=True, use_reloader=False))
+    # このportでアプリケーションを起動
+    port = 5004
+
+    # なんらかの原因でアプリケーションが終了しなかった場合に備えて
+    # このportで起動してるアプリケーションを強制終了
+    os.system(f"lsof -i:{port} | grep LISTEN | awk '{{print $2}}' | xargs kill")
+    
+
+    # debug= Trueだとコンパイル時にエラーが出る
+    thread = threading.Thread(target=app.run, kwargs={'port': port, 'debug': False, 'use_reloader': False})
     thread.daemon = True
     thread.start()
 
@@ -59,6 +78,6 @@ if __name__ == '__main__':
     webview.create_window("診療申込書作成アプリ", f"http://localhost:{port}")
     webview.start()
 
+
     # uploadフォルダ内のファイルを削除
-    
     shutil.rmtree(UPLOAD_FOLDER)
